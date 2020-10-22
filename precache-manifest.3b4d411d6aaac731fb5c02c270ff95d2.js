061e3a4d33da430436bd864a74861181801c1bcc
Kumar Nikhil <knikhil352000@gmail.com>
2020-10-23 05:14:10 +0530

    Updates

---
+++ precache-manifest.3b4d411d6aaac731fb5c02c270ff95d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4848aa12f45bb3dcb9055b15819b3792",
+    "url": "/React-Destination/index.html"
+  },
+  {
+    "revision": "e9477294b7a6e0bc1786",
+    "url": "/React-Destination/static/css/main.a40d25a3.chunk.css"
+  },
+  {
+    "revision": "6175e63fbe1139b84a70",
+    "url": "/React-Destination/static/js/2.e53fe2e4.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/React-Destination/static/js/2.e53fe2e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9477294b7a6e0bc1786",
+    "url": "/React-Destination/static/js/main.35e23446.chunk.js"
+  },
+  {
+    "revision": "23968a2cf25691fc0c2e",
+    "url": "/React-Destination/static/js/runtime-main.67b97376.js"
+  }
+]);
