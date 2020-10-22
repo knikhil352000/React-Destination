190129138586c8d6dfdb3726375d6c785baf3731
Kumar Nikhil <knikhil352000@gmail.com>
2020-10-23 03:52:50 +0530

    Added Route to Each nav-link

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import '../../App.css';
 import React from 'react';
 import HeroSection from '../HeroSection';
+import Cards from '../Cards';
 function Home(){
     return(
         <>
             <HeroSection />
+            <Cards />
         </>
     )
 }
